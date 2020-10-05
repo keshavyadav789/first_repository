85acebc65b8944edf87576bf60ad2cef41ca227c
keshavyadav789 <65011436+keshavyadav789@users.noreply.github.com>
2020-10-05 13:28:07 +0530

    Add files via upload

---
+++ strings_in_python.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 21:27:30 2020
+
+@author: sahil
+"""
+
+string="include"
+print(string)
+string+="help"
+print(string)
+
+
+#ways fo rdefining strings
+string1="keshav"
+print(string1)
+string2='sahil'
+print(string2)
+
+#program to access strings
+string="include help"
+print(string[5])
+print("include help"[5])
+print(string[-1])
+print(string[12])
